9d10a810d57ba29c0f5495cd4d3086e12a1c40f1
Jaime <67835355+jaimemodestojr@users.noreply.github.com>
2021-08-13 01:28:14 -0300

    Update TesteTecnico.py

--- TesteTecnico.py
+++ TesteTecnico.py
@@ -1,23 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
+'''
+1) Desenvolva um script em python para retornar o primeiro valor não repetido de uma string.
 
-# 1) Desenvolva um script em python para retornar o primeiro valor não repetido de uma string.
-# 
-# Exemplo:
-# input -> 'teste'
-# output -> 's'
-# input -> 'engenharia de dados'
-# output -> 'g'
+Exemplo:
+input -> 'teste'
+output -> 's'
+input -> 'engenharia de dados'
+output -> 'g'
+'''
 
-# In[16]:
+# Script:
 
 
 # Primeiramente, foi criada uma função, que será chamada mais tarde
-# Essa função tem por responsabilidade percorrer toda a string e diferenciar as letras na dada palavra ou frase inserida e,
-#por fim, acusar àquela primeira que não possui repetição
 
-# Caso só existam letras que possuem repetição, a função retornará que não existem palavras ou letras únicas, isto é, 
-#sem repetição, na palavra ou frase inserida.
+# Essa função tem por responsabilidade percorrer toda a string e diferenciar as letras na dada palavra ou frase inserida e, por fim, acusar àquela primeira que não possui repetição
+
+# Caso só existam letras que possuem repetição, a função retornará que não existem palavras ou letras únicas, isto é, sem repetição, na palavra ou frase inserida.
 
 def first_letter_without_repetition(string):
     letters_order = []
@@ -33,36 +31,35 @@ def first_letter_without_repetition(string):
             return i
     return ("Não há letras únicas, isto é, sem repetição, na palavra ou frase inserida.")
 
-# Agora, chama-se a função first_letter_without_repetition, ao mesmo tempo que se pede ao usuário que insira uma palavra 
-#ou frase e, para o caso do mesmo inserir um número, str converte imediatamente o mesmo para string
-# Também colocasse o .lower() para minimizar todas as letras inseridas pelo usuário, para que o script considere todas
-#as letras igualmente, pois o Python é case sensitive
+# Agora, chama-se a função first_letter_without_repetition, ao mesmo tempo que se pede ao usuário que insira uma palavra ou frase e, para o caso do mesmo inserir um número, str converte imediatamente o mesmo para string
+
+# Também colocasse o .lower() para minimizar todas as letras inseridas pelo usuário, para que o script considere todas  as letras igualmente, pois o Python é case sensitive
 
 print(first_letter_without_repetition(input("Digite uma palavra ou frase aqui: ")))
 
-
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+'''
+# 2) O conjunto de dados flor Íris ou conjunto de dados Íris de Fisher é um conjunto de dados multivariados introduzido pelo estatístico e biólogo britânico Ronald Fisher em seu artigo de 1936. 
+Desenvolva um script em python para encontrar a média do petal_length de cada espécie presente no dataset. 
+Utilize a função pd.read_csv() para baixar o conteúdo disponível na url https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv
+'''
 
-# 2) O conjunto de dados flor Íris ou conjunto de dados Íris de Fisher é um conjunto de dados multivariados introduzido pelo estatístico e biólogo britânico Ronald Fisher em seu artigo de 1936. Desenvolva um script em python para encontrar a média do petal_length de cada espécie presente no dataset. Utilize a função pd.read_csv() para baixar o conteúdo disponível na url https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv
+# Script:
 
-# In[12]:
+# Primeiro, importam-se as bibliotecas necessárias para o script
 
-
-# Primeiro, importaremos as bibliotecas necessárias para o script
 import pandas as pd
 import numpy as np
 
+# Usa-se a biblioteca pandas para importa o dataset, em csv, para o nosso script
 
-# In[13]:
-
-
-# Usaremos a biblioteca pandas para importarmos o dataset, em csv, para o nosso script
 flowers = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
 
 print(flowers)
 
-# Agora, separemos as espécies em um set separado, apenas para conferir quantas espécies diferentes existem no dataset, apenas para fins de análise e conferência
+# Agora, separam-se as espécies em um set, apenas para conferir quantas espécies diferentes existem no dataset, para fins de análise e conferência
+
 species = set(flowers.species)
 
 print(species)
@@ -71,17 +68,12 @@ number_of_species = len(set(flowers.species))
 
 print(number_of_species)
 
-# Agora, utilizaremos o summary, da biblioteca numpy, para mostrarmos um apanhado estatístico geral do dataset
+# Agora, utiliza-se o summary, da biblioteca numpy, para mostrar um apanhado estatístico geral do dataset
+
 summary = flowers.describe()
 
 print(summary)
 
-# Agora, finalmente, agrupados o dataset nas 3 espécies vistas anteriormente e calculamos a média de suas colunas, incluindo a requisitada petal_length
+# Agora, finalmente, agrupa-se o dataset nas 3 espécies vistas anteriormente e calcula-se a média de suas colunas, incluindo a requisitada petal_length
+
 flowers.groupby("species").agg(["mean"])
-
-
-# In[ ]:
-
-
-
-
